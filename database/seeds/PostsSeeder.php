194e2b154f17f35360d89734c642e6d8084dc7a3
Edoardo <a@Air-di-edo.lan>
2019-10-17 17:24:26 +0200

    creati post fittizi

--- database/seeds/PostsSeeder.php
+++ database/seeds/PostsSeeder.php
@@ -1,16 +1,16 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
-class DatabaseSeeder extends Seeder
+use App\Post;
+class PostsSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(PostsSeeder::class);
+        factory(Post::class, 30)->create();
     }
 }
